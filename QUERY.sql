9c957023def4803390cae6aa47165a44829c17b6
Ahmed-Nakib <office.nakib@gmail.com>
2026-06-14 17:32:46 +0200

    query 3

--- QUERY.sql
+++ QUERY.sql
@@ -106,3 +106,14 @@ SELECT user_id, full_name, email
 FROM users
 WHERE full_name ILIKE 'Tanvir%'
    OR full_name ILIKE '%Haque%';
+
+
+-- Query 3:
+
+SELECT
+    booking_id,
+    user_id,
+    match_id,
+    COALESCE(payment_status, 'Action Required') AS systematic_status
+FROM Bookings
+WHERE payment_status IS NULL;
